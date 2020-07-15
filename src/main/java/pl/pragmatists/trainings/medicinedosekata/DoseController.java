c7d986316e5a7e6bce41c82c693071b3e35c2a8f
bukowmic <bukowmic@gmail.com>
2020-07-15 12:37:33 +0200

    całość

--- src/main/java/pl/pragmatists/trainings/medicinedosekata/DoseController.java
+++ src/main/java/pl/pragmatists/trainings/medicinedosekata/DoseController.java
@@ -1,17 +1,72 @@
 package pl.pragmatists.trainings.medicinedosekata;
 
-import pl.pragmatists.trainings.medicinedosekata.dependencies.AlertService;
-import pl.pragmatists.trainings.medicinedosekata.dependencies.HealthMonitor;
-import pl.pragmatists.trainings.medicinedosekata.dependencies.MedicinePump;
+import pl.pragmatists.trainings.medicinedosekata.dependencies.*;
 
 public class DoseController {
 
-    public DoseController(HealthMonitor healthMonitor, MedicinePump medicinePump, AlertService alertService) {
+    private final HealthMonitor healthMonitor;
+    private final MedicinePump medicinePump;
+    private final AlertService alertService;
 
+    public DoseController(HealthMonitor healthMonitor, MedicinePump medicinePump, AlertService alertService) {
+        this.healthMonitor = healthMonitor;
+        this.medicinePump = medicinePump;
+        this.alertService = alertService;
     }
 
     public void checkHealthAndApplyMedicine() {
-
+        int systolicBloodPressure = healthMonitor.getSystolicBloodPressure();
+        if (alarmingLowPressure(systolicBloodPressure)) {
+            alertService.notifyDoctor();
+            raisePressure();
+        }
+        if (veryLow(systolicBloodPressure)) {
+            raisePressure();
+        }
+        if (lowPressure(systolicBloodPressure)) {
+            raisePressure();
+        }
+        if (highPressure(systolicBloodPressure)) {
+            lowPressure();
+        }
     }
 
+    private boolean alarmingLowPressure(int systolicBloodPressure) {
+        return systolicBloodPressure < 55;
+    }
+
+    private boolean highPressure(int systolicBloodPressure) {
+        return systolicBloodPressure > 150;
+    }
+
+    private boolean lowPressure(int systolicBloodPressure) {
+        return systolicBloodPressure < 90;
+    }
+
+    private boolean veryLow(int systolicBloodPressure) {
+        return systolicBloodPressure < 60;
+    }
+
+    private void lowPressure() {
+        tryDose(Medicine.PRESSURE_LOWERING_MEDICINE);
+    }
+
+    private void raisePressure() {
+        tryDose(Medicine.PRESSURE_RAISING_MEDICINE);
+    }
+
+    private void tryDose(Medicine medicine) {
+        if (canDoseMedicine(medicine)) {
+            try {
+                medicinePump.dose(medicine);
+            } catch (DoseUnsuccessfulException exception) {
+                medicinePump.dose(medicine);
+            }
+        }
+    }
+
+    private boolean canDoseMedicine(Medicine medicine) {
+        int timeSinceLastDoseInMinutes = medicinePump.getTimeSinceLastDoseInMinutes(medicine);
+        return timeSinceLastDoseInMinutes > 30;
+    }
 }
--- src/test/java/pl/pragmatists/trainings/medicinedosekata/MedicineDosingTest.java
+++ src/test/java/pl/pragmatists/trainings/medicinedosekata/MedicineDosingTest.java
@@ -1,13 +1,90 @@
 package pl.pragmatists.trainings.medicinedosekata;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import pl.pragmatists.trainings.medicinedosekata.dependencies.*;
 
-public class MedicineDosingTest {
+import static org.mockito.Mockito.*;
 
-    @Test
-    public void __start__here() {
+class MedicineDosingTest {
+    private HealthMonitor healthMonitor;
+    private MedicinePump medicinePump;
+    private AlertService alertService;
+    private DoseController controller;
 
+    @BeforeEach
+    void setUp() {
+        healthMonitor = mock(HealthMonitor.class);
+        medicinePump = mock(MedicinePump.class);
+        alertService = mock(AlertService.class);
+        controller = new DoseController(healthMonitor, medicinePump, alertService);
+        when(medicinePump.getTimeSinceLastDoseInMinutes(any())).thenReturn(31);
     }
 
+    @Test
+    void should_dose_medicine_when_blood_pressure_below_90() {
+        when(healthMonitor.getSystolicBloodPressure()).thenReturn(89);
+
+        controller.checkHealthAndApplyMedicine();
+
+        verify(medicinePump).dose(Medicine.PRESSURE_RAISING_MEDICINE);
+    }
+
+    @Test
+    void should_no_dose_pressure_raising_medicine_when_blood_pressure_above_90() {
+        when(healthMonitor.getSystolicBloodPressure()).thenReturn(91);
+
+        controller.checkHealthAndApplyMedicine();
+
+        verify(medicinePump, never()).dose(Medicine.PRESSURE_RAISING_MEDICINE);
+    }
+
+    @Test
+    void should_dose_two_pressure_raising_medicine_when_blood_pressure_below_60() {
+        when(healthMonitor.getSystolicBloodPressure()).thenReturn(59);
+
+        controller.checkHealthAndApplyMedicine();
+
+        verify(medicinePump, times(2)).dose(Medicine.PRESSURE_RAISING_MEDICINE);
+    }
+
+    @Test
+    void should_dose_pressure_lowering_medicine_when_blood_pressure_above_150() {
+        when(healthMonitor.getSystolicBloodPressure()).thenReturn(151);
+
+        controller.checkHealthAndApplyMedicine();
+
+        verify(medicinePump).dose(Medicine.PRESSURE_LOWERING_MEDICINE);
+    }
+
+    @Test
+    void should_try_another_dose_medicine_when_pump_is_not_working() {
+        when(healthMonitor.getSystolicBloodPressure()).thenReturn(89);
+        doThrow(DoseUnsuccessfulException.class).doNothing().when(medicinePump).dose(any());
+
+        controller.checkHealthAndApplyMedicine();
+
+        verify(medicinePump, times(2)).dose(any());
+    }
+
+    @Test
+    void should_no_dose_medicine_when_last_dose_was_earlier_than_30min_ago() {
+        when(healthMonitor.getSystolicBloodPressure()).thenReturn(89);
+        when(medicinePump.getTimeSinceLastDoseInMinutes(Medicine.PRESSURE_RAISING_MEDICINE)).thenReturn(20);
+
+        controller.checkHealthAndApplyMedicine();
+
+        verify(medicinePump, never()).dose(Medicine.PRESSURE_RAISING_MEDICINE);
+    }
+
+    @Test
+    void should_send_alarm_and_dose_three_pressure_raising_medicine_when_blood_pressure_below_55() {
+        when(healthMonitor.getSystolicBloodPressure()).thenReturn(54);
+
+        controller.checkHealthAndApplyMedicine();
+
+        verify(alertService).notifyDoctor();
+        verify(medicinePump, times(3)).dose(Medicine.PRESSURE_RAISING_MEDICINE);
+    }
 }
